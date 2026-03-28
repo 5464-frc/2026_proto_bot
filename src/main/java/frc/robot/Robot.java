c721b2b7914b270b6d0c99b8412c8671540251dc
FRC5464 <bluejacketprogramming@gmail.com>
2026-03-28 16:38:05 -0500

    more copy

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -71,12 +71,12 @@ public class Robot extends TimedRobot
   @Override
   public void robotPeriodic()
   {
-    m_SwerveSubsystem.periodic();
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    m_robotContainer.periodic();
   }
 
   /**
