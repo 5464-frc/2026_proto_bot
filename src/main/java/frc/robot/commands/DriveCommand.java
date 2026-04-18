dfb6a1abf278763b1f5b984a00caf806faa818ad
FRC5464 <bluejacketprogramming@gmail.com>
2026-04-18 14:48:09 -0500

    drive tweaks

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -23,14 +23,14 @@ public class DriveCommand extends Command{
     @Override
     public void execute() {
         // introduce deadband to keep controller drift from causing issues
-        double driveX = shtick.getRawAxis(1);
-        double driveY = shtick.getRawAxis(0);
+        double driveX = shtick.getRawAxis(0);
+        double driveY = shtick.getRawAxis(1);
         double driveRot = -shtick.getRawAxis(4);
         if(Math.abs(driveX) < 0.1){ driveX = 0;}
         if(Math.abs(driveY) < 0.1){ driveY = 0;}
         if(Math.abs(driveRot) < 0.1){ driveRot = 0;}
 
-        swurv.drive(driveX, driveY, driveRot);
+        swurv.drive(-driveX, driveY, driveRot);
         // System.out.println("driving yo");
     }
 
